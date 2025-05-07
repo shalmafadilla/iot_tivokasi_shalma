6e9ed00f0f347e395c124e60637fec4865eadcc1
shalmanaff <shalmafadilla28@gmail.com>
2025-05-07 16:11:26 +0700

    upload praktik bab 15

---
+++ minggu 12/praktik_bab15_iot/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GraphController;
+
+
+Route::get('/', [GraphController::class, 'index'])->name('graph');
+Route::get('/graph/export', [GraphController::class, 'exportToExcel'])->name('graph.export');  // Pastikan rute ini ada
